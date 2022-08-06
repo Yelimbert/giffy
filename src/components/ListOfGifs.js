a17ae9372a5203a294a9c8e95a07d0d571754063
Yelimbert Javier <yelimbertjavier@MacBook-Pro-de-Yelimbert.local>
2022-08-06 11:49:24 -0400

    show gifs

---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,21 @@
+import Gif from "./Gif";
+import React, {useEffect, useState} from 'react';
+import getGifs from "../services/getGifs";
+
+export default function ListOfGifs({ keyword }) {
+    const [gifs, setGifs] = useState([])
+
+    useEffect(function() {
+        getGifs({ keyword  })
+            .then(gifs => setGifs(gifs))
+      }, [keyword])
+
+    return gifs.map(({id, title, url}) => 
+        <Gif 
+            key={id}
+            title={title} 
+            url={url} 
+            id={id}
+            />
+        )
+}
---
+++ src/services/getGifs.js
@@ -0,0 +1,19 @@
+const apiKey = 'r5cTWyYs2WAjHq3KiDIjVrIeJTnfalaV';
+
+
+export default function getGifs({keyword = 'Lion'} = {}) {
+    const APIurl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`
+    return fetch(APIurl)
+    .then(res => res.json())
+    .then(response => {
+      const {data = []} = response
+      if (Array.isArray(data)) {
+          const gifs = data.map(image => {
+              const {images, title, id} = image
+              const { url } = images.downsized_medium
+              return {title, id, url}
+          });
+        return gifs
+      }
+    })
+}
